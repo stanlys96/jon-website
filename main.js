080d37cfa874636148c571cfd4a70e6e33c2f28d
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2022-01-21 11:47:53 +0700

    add animation

--- main.js
+++ main.js
@@ -70,3 +70,26 @@ menuLinks.forEach(link => {
   });
 });
 
+let letters = nameBtn.textContent.split("");
+nameBtn.textContent = "";
+letters.forEach((letter, i) => {
+  i += 1;
+  let span = document.createElement("span");
+  let delay = i / 20;
+  if (i % 2 === 0) {
+    delay -= 0.1;
+  } else {
+    delay += 0.05;
+  }
+  let letterOut = document.createElement("span");
+  letterOut.textContent = letter;
+  letterOut.style.transitionDelay = `${delay}s`;
+  letterOut.classList.add("out");
+  span.append(letterOut);
+  let letterIn = document.createElement("span");
+  letterIn.textContent = letter;
+  letterIn.style.transitionDelay = `${delay}s`;
+  letterIn.classList.add("in");
+  span.append(letterIn);
+  nameBtn.append(span);
+})
