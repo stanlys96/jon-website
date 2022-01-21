94518983ea5d23fd46665da88b868fdbefa18bf0
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2022-01-21 11:32:31 +0700

    nambah animasi text

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const newsBtn = document.querySelector('.news-btn');
 const newsSection = document.getElementById('news');
 const midSection = document.getElementById('mid');
 const nameBtn = document.querySelector('.web-name');
+const menuLinks = document.querySelectorAll('.menu-a');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
@@ -42,3 +43,30 @@ nameBtn.addEventListener('click', () => {
     }, 350)
   }, 350);
 });
+
+menuLinks.forEach(link => {
+  let letters = link.textContent.split("");
+  link.textContent = "";
+  letters.forEach((letter, i) => {
+    i += 1;
+    let span = document.createElement("span");
+    let delay = i / 20;
+    if (i % 2 === 0) {
+      delay -= 0.1;
+    } else {
+      delay += 0.05;
+    }
+    let letterOut = document.createElement("span");
+    letterOut.textContent = letter;
+    letterOut.style.transitionDelay = `${delay}s`;
+    letterOut.classList.add("out");
+    span.append(letterOut);
+    let letterIn = document.createElement("span");
+    letterIn.textContent = letter;
+    letterIn.style.transitionDelay = `${delay}s`;
+    letterIn.classList.add("in");
+    span.append(letterIn);
+    link.append(span);
+  });
+});
+
